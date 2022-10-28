703e4d81eca2ed9a58f1cc7a612e77d01564be68
wibbleydock <pzns1337@gmail.com>
2022-10-28 16:54:52 +0300

    [MODIFY] modify parsing script and add scraping

--- parser.py
+++ parser.py
@@ -1,11 +1,6 @@
 from typing import List, Union
+from item import Item
 from abc import ABC, abstractclassmethod
-
-class Item:
-    def __init__(self, product_name: str, price: Union[float, int], url: str, pic_url: str) -> None:
-        self.product_name = product_name
-        self.price = price
-        self.url = url
 
 class Parser(ABC):
     @abstractclassmethod
--- technopark_parser.py
+++ technopark_parser.py
@@ -1,5 +1,7 @@
+import re
 from typing import Union
 from parser import Parser
+from item import Item
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,14 +15,24 @@ class TechnoparkParser(Parser):
         self.price = price
         self.url = url
 
-    def scraping(self) -> str:
+    def scraping(self):
         driver = webdriver.Chrome(ChromeDriverManager.install())
         driver.get(self.url)
         searchbar = driver.find_element(By.XPATH, '//*[@id="header-search-input-main"]')
         searchbar.send_keys(self.url + Keys.RETURN)
         search = driver.find_element(By.CLASS_NAME, 'header-search-input__buttons')
         search.click()
-        title = soup.find_all(attrs={"class":{"product-card-big__name"}})
-        price = soup.find_all(attrs={"class":{"product-prices__price"}})
-        url = "https://technopark.ru" + soup.find(attrs={"class":{"href"}})
-        picture = soup.find(attrs={"class":{"tp-lazy-image product-card-image__img"}})
+        
+        case_data = soup.find_all('div', attrs={'class':'product-card-big__container'})
+        item_data = []
+
+        for data in case_data:
+
+            title = soup.find(attrs={"class":{"product-card-big__name"}}).get_text()
+            price = soup.find_all(attrs={"class":{"product-prices__price"}}).get_text()
+            url = "https://technopark.ru" + soup.find(attrs={"class":{"href"}})
+            picture = soup.find(attrs={"class":{"tp-lazy-image product-card-image__img"}})
+        
+        item_data.append(Item(product_name=title, price=price, url=url, pic_url=picture))
+
+        return item_data
