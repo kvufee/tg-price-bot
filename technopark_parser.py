54eef390abe7951db35fd79a8859e011b191a177
wibbleydock <pzns1337@gmail.com>
2022-10-31 14:37:35 +0300

    [MODIFY] rework scraping def and remove unused imports

--- technopark_parser.py
+++ technopark_parser.py
@@ -1,38 +1,41 @@
-import re
 from typing import Union
 from parser import Parser
 from item import Item
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 class TechnoparkParser(Parser):
+
     def __init__(self, product_name: str, price: Union[float, int], url: str) -> None:
+        
         self.product_name = product_name
         self.price = price
-        self.url = url
+        self.url = 'https://technopark.ru'
 
     def scraping(self):
-        driver = webdriver.Chrome(ChromeDriverManager.install())
+
+        driver = webdriver.Chrome(ChromeDriverManager().install())
         driver.get(self.url)
         searchbar = driver.find_element(By.XPATH, '//*[@id="header-search-input-main"]')
-        searchbar.send_keys(self.url + Keys.RETURN)
-        search = driver.find_element(By.CLASS_NAME, 'header-search-input__buttons')
-        search.click()
+        searchbar.send_keys(self.product_name)
+        searchbar.send_keys(Keys.RETURN)
+
+        page_content = driver.page_source
+        soup = BeautifulSoup(page_content, 'lxml')
+
+        containers = soup.find_all('div', attrs={'class':'product-card-big__container'})
         
-        case_data = soup.find_all('div', attrs={'class':'product-card-big__container'})
-        item_data = []
+        items = []
+        for container in containers:
+            
+            url = 'https://technopark.ru' + container.find('a')['href']
+            title = container.find('a')['title']
+            price = container.find('div', attrs={'class':'product-prices__price'}).get_text()
+            pic = container.find('img', attrs={'class':'tp-lazy-image product-card-image__img'})
 
-        for data in case_data:
-
-            title = soup.find(attrs={"class":{"product-card-big__name"}}).get_text()
-            price = soup.find_all(attrs={"class":{"product-prices__price"}}).get_text()
-            url = "https://technopark.ru" + soup.find(attrs={"class":{"href"}})
-            picture = soup.find(attrs={"class":{"tp-lazy-image product-card-image__img"}})
+            items.append(Item(url=url, product_name=title, price=price, pic_url = pic))
         
-        item_data.append(Item(product_name=title, price=price, url=url, pic_url=picture))
-
-        return item_data
+        return(items)
