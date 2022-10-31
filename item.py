99b1454d2b6735fe19f50fd7a86638e6b3bc6489
wibbleydock <pzns1337@gmail.com>
2022-10-31 14:28:23 +0300

    [MODIFY] modify item order

--- item.py
+++ item.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 class Item:
-    def __init__(self, product_name: str, price: Union[float, int], url: str, pic_url: str) -> None:
+    def __init__(self, url: str, product_name: str, price: Union[float, int], pic_url: str) -> None:
         self.product_name = product_name
         self.price = price
         self.url = url
