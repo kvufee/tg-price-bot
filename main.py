c1b9fd41ac7de6db42e3114906daa4d2cf3789b7
wibbleydock <pzns1337@gmail.com>
2022-11-13 23:34:29 +0300

    [MODIFY] work on bot

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 import telebot
 
-from telebot import types
-
-from config import TOKEN
+from configuration.config import TOKEN
 
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
+def start_message(message) -> None:
+    
     bot.send_message(message.chat.id, 'Greetings🤨')
-
-@bot.message_handler(commands=['button'])
-def option_button(message):
-    markup = types.ReplyKeyboardMarkup(row_width = 2)
-    itembtn1 = types.KeyboardButton('Search')
-    itembtn2 = types.KeyboardButton('Log')
-    itembtn3 = types.KeyboardButton('Favourite')
-    itembtn4 = types.KeyboardButton('About')
-
-    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
-    markup = types.ReplyKeyboardRemove(selective=False)
-    bot.send_message(message.chat.id, "How can I help you?", reply_markup=markup)
 
 
 bot.infinity_polling()
