624bda03ebe1300279beace5840b24db0c2da315
wibbleydock <pzns1337@gmail.com>
2022-11-16 01:23:55 +0300

    [MODIFY] add handle message function, add match/case structure

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 import telebot
 
+from telebot import types
+
 from configuration.config import TOKEN
+from parsers.technopark_parser import TechnoparkParser
 
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start_message(message) -> None:
-    
-    bot.send_message(message.chat.id, 'Greetings🤨')
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message) -> None:
+    markup = keyboard_markup()
+
+    bot.send_message(message.chat.id, 'Greetings🤨', reply_markup=markup)
+    bot.register_next_step_handler(message, handle_message)
+
+
+def keyboard_markup() -> types.ReplyKeyboardMarkup:
+    markup = types.ReplyKeyboardMarkup()
+
+    tp_button = types.KeyboardButton('Technopark')
+    log_button = types.KeyboardButton('Search log')
+
+    markup.add(tp_button, log_button)
+
+    return markup
+
+
+def handle_message(message):
+    match message.txt:
+
+
+        case 'Technopark':
+            bot.send_message(message.chat.id, 'What do you want to find?', reply_markup=types.ReplyKeyboardRemove())
+            bot.register_next_step_handler(message, technopark_script)
+
+        case 'Search log':
+            bot.send_message(message.chat.id, reply_markup=types.ReplyKeyboardRemove())
+            bot.register_next_step_handler(message, log_script)
+
+
+def technopark_script(message):
+    
+
+    return 
+
+def log_script(message):
+
+
+    return
 
 bot.infinity_polling()
