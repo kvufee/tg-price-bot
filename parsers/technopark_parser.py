5a89d025ebbe13711ea6a696cf2365063219a0e8
wibbleydock <pzns1337@gmail.com>
2022-11-17 01:34:03 +0300

    [MODIFY] rework __init__ and add sleep to script

--- parsers/technopark_parser.py
+++ parsers/technopark_parser.py
@@ -1,4 +1,5 @@
-from typing import Union
+from typing import Union, List
+import time
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -6,28 +7,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-from parsers.parser import Parser
+from .parser import Parser
 from item import Item
 
 
 class TechnoparkParser(Parser):
 
-
-    def __init__(self,
-                 product_name: str,
-                 price: Union[float, int],
-                 url: str) -> None:
-        self.product_name = product_name
-        self.price = price
+    def __init__(self) -> None:
         self.url = 'https://technopark.ru'
 
 
-    def scraping(self):
+    def scraping(self, product_name: str) -> List[str]:
         driver = webdriver.Chrome(ChromeDriverManager().install())
         driver.get(self.url)
         searchbar = driver.find_element(By.XPATH, '//*[@id="header-search-input-main"]')
-        searchbar.send_keys(self.product_name)
+        searchbar.send_keys(product_name)
         searchbar.send_keys(Keys.RETURN)
+
+        time.sleep(5)
 
         page_content = driver.page_source
         soup = BeautifulSoup(page_content, 'lxml')
@@ -39,9 +36,9 @@ class TechnoparkParser(Parser):
         for container in containers:
             url = 'https://technopark.ru' + container.find('a')['href']
             title = container.find('a')['title']
-            price = container.find('div', attrs={'class':'product-prices__price'}).get_text()
+            price = container.find('div', attrs={'class':'product-prices__price'}).get_text()[4:]
             pic = container.find('img', attrs={'class':'tp-lazy-image product-card-image__img'})
 
-            items.append(Item(url=url, product_name=title, price=price, pic_url = pic))
+            items.append(Item(url=url, product_name=title, price=price, pic_url=pic))
         
         return items
