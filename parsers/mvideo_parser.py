94184dadadcbd0d569bc3fa81f108f5cc61fb62e
wibbleydock <pzns1337@gmail.com>
2023-03-06 23:43:17 +0300

    [MODIFY] add webdriver script and start writing scraper

--- parsers/mvideo_parser.py
+++ parsers/mvideo_parser.py
@@ -18,3 +18,24 @@ class MvideoParser(Parser):
         self.url = 'https://www.mvideo.ru/'
     
     def scraping(self, product_name: str) -> List[Item]:
+        options = Options()
+
+        with webdriver.Chrome(ChromeDriverManager().install()) as driver:
+            driver.get(self.url)
+
+            time.sleep(cfg.SLEEP_DURATION)
+
+            searchbar = driver.find_element(By.XPATH, '//*[@id="1"]')
+            searchbar.send_keys(product_name)
+            searchbar.send_keys(Keys.RETURN)
+
+            page_content = driver.page_source
+
+        soup = BeautifulSoup(page_content, 'lxml')
+
+        containers = soup.find_all('div', attrs={'class':'ng-star-inserted product-cards-row'})
+
+        items = []
+
+        for container in containers:
+            url = self.url + container.find('a')
